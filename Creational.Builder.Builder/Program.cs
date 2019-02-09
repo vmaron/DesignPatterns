8897cffdbe22b20703ca121cd30da41b39ee5932
Vyacheslav Maron <vmaronmail@gmail.com>
2019-02-09 11:44:17 -0500

    Added Factories and Builder design patterns

---
+++ Creational.Builder.Builder/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creational.Builder.Builder
+{
+    internal class HtmlElement
+    {
+        private const int IndentSize = 2;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        public string Name, Text;
+
+        public HtmlElement()
+        {
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', IndentSize * indent);
+            sb.Append($"{i}<{Name}>\n");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', IndentSize * (indent + 1)));
+                sb.Append(Text);
+                sb.Append("\n");
+            }
+
+            foreach (var e in Elements)
+                sb.Append(e.ToStringImpl(indent + 1));
+
+            sb.Append($"{i}</{Name}>\n");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    internal class HtmlBuilder
+    {
+        private readonly string _rootName;
+
+        private HtmlElement _root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this._rootName = rootName;
+            _root.Name = rootName;
+        }
+
+        // not fluent
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            _root.Elements.Add(e);
+        }
+
+        public HtmlBuilder AddChildFluent(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            _root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _root.ToString();
+        }
+
+        public void Clear()
+        {
+            _root = new HtmlElement {Name = _rootName};
+        }
+    }
+
+    public class Demo
+    {
+        private static void Main(string[] args)
+        {
+            // if you want to build a simple HTML paragraph using StringBuilder
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            // now I want an HTML list with 2 words in it
+            var words = new[] {"hello", "world"};
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words) sb.AppendFormat("<li>{0}</li>", word);
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
+            // ordinary non-fluent builder
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+
+            // fluent builder
+            sb.Clear();
+            builder.Clear(); // disengage builder from the object it's building, then...
+            builder.AddChildFluent("li", "hello").AddChildFluent("li", "world");
+            Console.WriteLine(builder);
+        }
+    }
+}
